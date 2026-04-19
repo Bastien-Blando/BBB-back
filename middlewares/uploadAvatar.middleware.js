541d5aafcb4511552d115f07737323b44dd75352
Bastien Blando <bastien.blando1@gmail.com>
2026-04-19 22:18:09 +0200

    Rewrite Cloudinary upload using memoryStorage + upload_stream

--- middlewares/uploadAvatar.middleware.js
+++ middlewares/uploadAvatar.middleware.js
@@ -1,6 +1,6 @@
 import { v2 as cloudinary } from 'cloudinary';
-import { CloudinaryStorage } from 'multer-storage-cloudinary';
 import multer from 'multer';
+import { Readable } from 'stream';
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -8,15 +8,30 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const storage = new CloudinaryStorage({
-  cloudinary,
-  params: {
-    folder: 'blablabook/avatars',
-    allowed_formats: ['jpg', 'jpeg', 'png', 'webp'],
-    transformation: [{ width: 300, height: 300, crop: 'fill' }],
-  },
-});
+const fileFilter = (_req, file, cb) => {
+  const allowedTypes = ['image/jpeg', 'image/png', 'image/webp'];
+  if (allowedTypes.includes(file.mimetype)) cb(null, true);
+  else cb(new Error('Type de fichier non autorisé'));
+};
 
-const avatarUpload = multer({ storage });
+// Multer stocke le fichier en mémoire (buffer)
+const upload = multer({ storage: multer.memoryStorage(), fileFilter });
 
+// Middleware qui upload le buffer vers Cloudinary après multer
+export function uploadToCloudinary(req, _res, next) {
+  if (!req.file) return next();
+
+  const stream = cloudinary.uploader.upload_stream(
+    { folder: 'blablabook/avatars', transformation: [{ width: 300, height: 300, crop: 'fill' }] },
+    (error, result) => {
+      if (error) return next(error);
+      req.file.path = result.secure_url;
+      next();
+    }
+  );
+
+  Readable.from(req.file.buffer).pipe(stream);
+}
+
+const avatarUpload = upload;
 export default avatarUpload;
--- routers/user.router.js
+++ routers/user.router.js
@@ -2,12 +2,11 @@ import { Router } from "express";
 import { userController } from "../controllers/index.js";
 import { userAuthentificationController } from "../controllers/index.js";
 import { authenticate } from '../middlewares/authentification.middleware.js';
-import avatarUpload from "../middlewares/uploadAvatar.middleware.js";
-// import upload from "../middlewares/uploadCover.middleware.js";
+import avatarUpload, { uploadToCloudinary } from "../middlewares/uploadAvatar.middleware.js";
 
 export const userRouter = Router();
 
-userRouter.post('/user/register', avatarUpload.single('avatar'), userAuthentificationController.register);
+userRouter.post('/user/register', avatarUpload.single('avatar'), uploadToCloudinary, userAuthentificationController.register);
 
 userRouter.post('/user/login', userAuthentificationController.login);
 
